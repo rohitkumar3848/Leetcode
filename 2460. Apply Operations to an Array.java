62e975965f9096e37b643904332eddec1063f82b
Rohit Kumar <125601198+rohitkumar3848@users.noreply.github.com>
2023-08-10 14:22:02 +0530

    Create 2460. Apply Operations to an Array.java

---
+++ 2460. Apply Operations to an Array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] applyOperations(int[] nums) {
+       int n=nums.length;
+       for(int i=0;i<n-1;i++){
+           if(nums[i]==nums[i+1]){
+               nums[i]*=2;
+               nums[i+1]=0;
+           }
+       }
+       for(int i=0,left=0;i<n;i++){
+           if(nums[i]!=0){
+               int temp=nums[i];
+               nums[i]=0;
+               nums[left]=temp;
+               left++;
+           }
+       }
+       return nums; 
+    }
+}
